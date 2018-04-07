9600d90fdef6c80c6863b540b7c8ec0199cf5ad8
shaunak <datarshaunak@gmail.com>
2018-04-08 00:59:22 +0530

    initial files and folders of git

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php
+/*
+This is demo project for attendance management system
+*/
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Attendance Management</title>
+</head>
+<body>
+	<form name="login_form">
+		<label>Email Id</label>
+		<input type="text" name="user_email_id">
+		<label>Password</label>
+		<input type="text" name="user_password">
+		<button>Login</button>
+	</form>
+</body>
+</html>
